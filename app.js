6d5f475580ce980e0f339268b4cedf207491b84f
Asghar Usama - Komvux Kärnan <usama.asghar@skola.helsingborg.se>
2019-10-29 13:04:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(() => {
+    $('.login-box h2').text('logga in');
+    $('.link1').children().text('H!');
+    $('.link2').children().text('HELLO!');
+    $('.link3').children().text('H!');
+   
+    });
+
+
